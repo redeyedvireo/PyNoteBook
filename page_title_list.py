1c8a9e76e44274d25ef6de2999b45cfe71d44a1b
Jeff <abc@abc.com>
2024-05-22 22:58:51 -0400

    Can store page titles in the Page Title List.

--- page_title_list.py
+++ page_title_list.py
@@ -1,5 +1,18 @@
 from PySide6 import QtCore, QtWidgets, QtGui
+from notebook_types import ENTITY_ID
+
 
 class CPageTitleList(QtWidgets.QListWidget):
   def __init__(self, parent):
     super(CPageTitleList, self).__init__(parent)
+
+  def addItems(self, pageDict):
+    for pageId, pageObj in pageDict.items():
+      self.addPageTitleItem(pageId, pageObj.m_title)
+
+  def addPageTitleItem(self, pageId: ENTITY_ID, pageTitle: str):
+	  # Add to the list
+    newItem = QtWidgets.QListWidgetItem(pageTitle)
+
+    newItem.setData(QtCore.Qt.ItemDataRole.UserRole, pageId)
+    self.addItem(newItem)
