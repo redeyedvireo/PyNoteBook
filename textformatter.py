e6b241828bf1ce0a8f87c67c043253c1992ec3f2
Jeff <abc@abc.com>
2024-08-05 21:03:36 -0400

    Continued refactoring.

--- textformatter.py
+++ textformatter.py
@@ -8,52 +8,54 @@ class TextFormatter:
     self.blockFormat = self.selectionCursor.blockFormat()
     self.tempCharFormat = QtGui.QTextCharFormat()
 
-  def finalize(self):
+  def mergeCharFormatAndfinalize(self):
+    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
+    self.textEdit.setTextCursor(self.selectionCursor)
+
+  def setBlockFormatAndFinalize(self):
+    self.selectionCursor.setBlockFormat(self.blockFormat)
+    self.textEdit.setTextCursor(self.selectionCursor)
+
+  def setCharFormatAndFinalize(self):
+    self.selectionCursor.setCharFormat(self.selectionFormat)
     self.textEdit.setTextCursor(self.selectionCursor)
 
   def _toggleStrikethrough(self):
     self.tempCharFormat.setFontStrikeOut(not self.selectionFormat.fontStrikeOut())
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   def _toggleUnderline(self):
     self.tempCharFormat.setFontUnderline(not self.selectionFormat.fontUnderline())
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   def _toggleItalic(self):
     self.tempCharFormat.setFontItalic(not self.selectionFormat.fontItalic())
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   def _toggleBold(self):
     if self.selectionFormat.fontWeight() != QtGui.QFont.Weight.Bold:
       self.tempCharFormat.setFontWeight(QtGui.QFont.Weight.Bold)
     else:
       self.tempCharFormat.setFontWeight(QtGui.QFont.Weight.Normal)
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+
+    self.mergeCharFormatAndfinalize()
 
   def _leftAlign(self):
     self.blockFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
-    self.selectionCursor.setBlockFormat(self.blockFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setBlockFormatAndFinalize()
 
   def _centerAlign(self):
     self.blockFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-    self.selectionCursor.setBlockFormat(self.blockFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setBlockFormatAndFinalize()
 
   def _rightAlign(self):
     self.blockFormat.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
-    self.selectionCursor.setBlockFormat(self.blockFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setBlockFormatAndFinalize()
 
   def _setTextColor(self, color: QtGui.QColor):
     self.tempCharFormat = QtGui.QTextCharFormat()
     self.tempCharFormat.setForeground(QtGui.QBrush(color))
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   def _noTextColor(self):
     # NOTE: This approach will cause all text in the selection to take on
@@ -61,14 +63,12 @@ class TextFormatter:
     # removing formatting changes within the block.  Unfortunately,
     # mergeCharFormat does not work when clearing a property.
     self.selectionFormat.clearForeground()
-    self.selectionCursor.setCharFormat(self.selectionFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setCharFormatAndFinalize()
 
   def _setBackgroundColor(self, color: QtGui.QColor):
     self.tempCharFormat = QtGui.QTextCharFormat()
     self.tempCharFormat.setBackground(QtGui.QBrush(color))
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   def _noBackgroundColor(self):
     # NOTE: This approach will cause all text in the selection to take on
@@ -76,18 +76,15 @@ class TextFormatter:
     # removing formatting changes within the block.  Unfortunately,
     # mergeCharFormat does not work when clearing a property.
     self.selectionFormat.clearBackground()
-    self.selectionCursor.setCharFormat(self.selectionFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setCharFormatAndFinalize()
 
   def _setFont(self, fontFamily: str):
     self.selectionFormat.setFontFamily(fontFamily)
-    self.selectionCursor.setCharFormat(self.selectionFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.setCharFormatAndFinalize()
 
   def _setFontSize(self, fontSize: int):
     self.tempCharFormat.setFontPointSize(fontSize)
-    self.selectionCursor.mergeCharFormat(self.tempCharFormat)
-    self.textEdit.setTextCursor(self.selectionCursor)
+    self.mergeCharFormatAndfinalize()
 
   @staticmethod
   def toggleStrikethrough(textEdit: QtWidgets.QTextEdit):
